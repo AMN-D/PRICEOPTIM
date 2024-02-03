4035ff07a23cb538b10836c40172212b19e846bd
AMANDDHUMAL <129135638+AMN-D@users.noreply.github.com>
2024-02-03 18:49:45 +0530

    remove many unecesary columns

--- optimisation.py
+++ optimisation.py
@@ -5,8 +5,9 @@ import pandas as pd
 df = pd.read_csv('amazon.csv')
 
 # remove non-usable columns
-df = df.drop(['img_link', 'product_link', 'user_id', 'user_name', 'review_id', 'review_title', 'review_content'], axis=1)
+df = df.drop(['about_product', 'product_id'], axis=1)
 
 # if you want to remove rows with missing values instead, you can use this line
 df = df.dropna()
+
 df.to_csv(r'amazon.csv', index=False)
