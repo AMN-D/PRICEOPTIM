b605f335cf54a49ebbbc7a38052e8b3e2447dfd1
AMANDDHUMAL <129135638+AMN-D@users.noreply.github.com>
2024-02-23 18:22:31 +0530

    Commiting

--- price_optimisation/files/static/script.js
+++ price_optimisation/files/static/script.js
@@ -109,3 +109,23 @@ function showBarChart(actualData, predictedData) {
     document.getElementById('visualization-btn').textContent = "Bar Chart ▼";
 }
 
+let lastScrollTop = 0;
+const header = document.querySelector('.base-header');
+const headerHeight = header.offsetHeight; // Get the height of the header
+const scrollThreshold = 100; // Adjust this value to set the scroll threshold
+
+window.addEventListener("scroll", function() {
+    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+    if (currentScroll > lastScrollTop && currentScroll > scrollThreshold) {
+        // Scroll down and below the threshold, hide the header
+        header.style.transform = "translateY(-100%)";
+        header.style.transition = "transform 0.3s ease"; // Add animation
+    } else {
+        // Scroll up or not yet reached the threshold, show the header
+        header.style.transform = "translateY(0)";
+        header.style.transition = "transform 0.3s ease"; // Add animation
+    }
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+}, false);
+
+
