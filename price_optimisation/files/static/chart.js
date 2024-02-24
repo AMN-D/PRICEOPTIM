535fcf2e8e91d014bac48408862d2ace8db1185c
AMANDDHUMAL <129135638+AMN-D@users.noreply.github.com>
2024-02-24 19:12:37 +0530

    created main landing page

--- price_optimisation/files/static/chart.js
+++ price_optimisation/files/static/chart.js
@@ -1,23 +1,182 @@
-function printSelectedValue(actualData, predictedData) {
-    // Get the selected value from the dropdown
-    var selectedValue = document.getElementById('visualSelector').value;
-    var selectedModelValue = document.getElementById('modelSelector').value;
+var currentChart = null;
 
-    if (selectedModelValue === '0') {
-        console.log("No model selected");
-        return; 
+function reloadVisualizationDisplay() {
+    if (currentChart) {
+        currentChart.destroy();
     }
+    var canvas = document.getElementById('plotCanvas');
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function pltBarChart(actualData, predictedData) {
+
+    actualData = JSON.parse(actualData);
+    predictedData = JSON.parse(predictedData);
 
     var canvas = document.getElementById('plotCanvas');
+    var ctx = canvas.getContext('2d');
 
-    if (selectedValue === '0') {
-        canvas.style.backgroundColor = 'red';
-        console.log("value 0:", actualData);
-    } else if (selectedValue === '1') {
-        canvas.style.backgroundColor = 'blue';
-        console.log("value 1:", predictedData);
-    } else if (selectedValue === '2') {
-        canvas.style.backgroundColor = 'green';
-        console.log("value 2:");
+    var canvasWidth = 700;
+    var canvasHeight = 600;
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+
+    var data = {
+        labels: actualData.map((value, index) => index), // Assuming index as labels
+        datasets: [{
+            label: 'Actual Data',
+            backgroundColor: 'rgba(207, 170, 1, 0.8)', // Adjust color as needed
+            data: actualData
+        }, {
+            label: 'Predicted Data',
+            backgroundColor: 'rgba(226, 226, 226, 0.8)', // Adjust color as needed
+            data: predictedData
+        }]
+    };
+    var options = {
+        scales: {
+            x: {
+                type: 'linear',
+                position: 'bottom'
+            },
+            y: {
+                type: 'linear',
+                position: 'left'
+            }
+        }
+    };
+
+    currentChart = new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: options
+    });
+
+}
+
+function pltLineplot(actualData, predictedData) {
+
+    actualData = JSON.parse(actualData);
+    predictedData = JSON.parse(predictedData);
+
+    var canvas = document.getElementById('plotCanvas');
+    var ctx = canvas.getContext('2d');
+
+    var canvasWidth = 700;
+    var canvasHeight = 600;
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+
+    var data = {
+        labels: actualData.map((value, index) => index), 
+        datasets: [{
+            label: 'Actual Data',
+            borderColor: 'rgba(207, 170, 1, 0.8)',
+            data: actualData
+        }, {
+            label: 'Predicted Data',
+            borderColor: 'rgba(54, 162, 235, 0.8)',
+            data: predictedData
+        }]
+    };
+
+    var options = {
+        scales: {
+            x: {
+                type: 'linear',
+                position: 'bottom'
+            },
+            y: {
+                type: 'linear',
+                position: 'left'
+            }
+        }
+    };
+
+    currentChart = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: options
+    });
+
+}
+
+function pltScatterplot(actualData, predictedData) {
+    // Parse actual and predicted data from string to array
+    actualData = JSON.parse(actualData);
+    predictedData = JSON.parse(predictedData);
+
+    // Get canvas element
+    var canvas = document.getElementById('plotCanvas');
+    var ctx = canvas.getContext('2d');
+    
+    var canvasWidth = 700;
+    var canvasHeight = 600;
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
+    
+    var data = {
+        datasets: [{
+            label: 'SCATTERPLOT',
+            backgroundColor: 'rgba(207, 170, 1, 0.8)',
+            data: actualData.map((value, index) => ({ x: value, y: predictedData[index] }))
+        }]
+    };
+
+    var options = {
+        scales: {
+            x: {
+                type: 'linear',
+                position: 'bottom'
+            },
+            y: {
+                type: 'linear',
+                position: 'left'
+            }
+        }
+    };
+
+    currentChart = new Chart(ctx, {
+        type: 'scatter',
+        data: data,
+        options: options
+    });
+
+}
+
+
+
+function printSelectedValue(actualData, predictedData) {
+    var selectedValue = document.getElementById('visualSelector').value;
+    var selectedModelValue = document.getElementById('modelSelector').value;
+    
+    reloadVisualizationDisplay();
+
+    if (selectedModelValue === '0') {
+        reloadVisualizationDisplay();
+        // Trigger a change event on the "visualSelector" element
+        const visualSelector = document.getElementById('visualSelector');
+        visualSelector.value = '0';
+        visualSelector.dispatchEvent(new Event('change'));
+    } else {
+        switch(selectedValue) {
+            case '0':
+                console.log("value 0:");
+                reloadVisualizationDisplay();
+                break;
+            case '1':
+                pltScatterplot(actualData, predictedData);
+                break;
+            case '2':
+                pltLineplot(actualData, predictedData);
+                break;
+            case '3':
+                pltBarChart(actualData, predictedData);
+                break;
+            default:
+                console.log("Invalid value");
+        }
     }
 }
+
--- price_optimisation/files/views.py
+++ price_optimisation/files/views.py
@@ -7,31 +7,26 @@ from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import load_model
 
 def homepage(request):
-    selected_model = request.GET.get('model', '')
 
-    if selected_model == '1':
-        df = pd.read_csv(os.path.join(settings.BASE_DIR, 'files', 'data', 'preprocessed_price_optimization_dataset.csv'))
-        model_path = os.path.join(settings.BASE_DIR, 'files', 'data', 'price_optimization_model.h5')
+    df = pd.read_csv(os.path.join(settings.BASE_DIR, 'files', 'data', 'preprocessed_price_optimization_dataset.csv'))
+    model_path = os.path.join(settings.BASE_DIR, 'files', 'data', 'price_optimization_model.h5')
 
-        X = df[['product_id', 'category_id', 'brand_encoded', 'historical_price_scaled']] 
-        y = df['historical_price'] 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X = df[['product_id', 'category_id', 'brand_encoded', 'historical_price_scaled']] 
+    y = df['historical_price'] 
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-        loaded_model = load_model(model_path)
+    loaded_model = load_model(model_path)
 
-        predictions = loaded_model.predict(X_test)
-        evaluation = loaded_model.evaluate(X_test, y_test)
+    predictions = loaded_model.predict(X_test)
+    evaluation = loaded_model.evaluate(X_test, y_test)
 
-        actual_data = list(y_test) 
-        predicted_data = list(predictions.flatten()) 
+    actual_data = list(y_test) 
+    predicted_data = list(predictions.flatten()) 
 
-        context = {
-            'actual_data': actual_data,
-            'predicted_data': predicted_data,
-        }
-    else:
-        print("no actual and predicted data")
-        context = {}
+    context = {
+        'actual_data': actual_data,
+        'predicted_data': predicted_data,
+    }
 
     return render(request, 'base.html', context)
 
