21e5e57f15b6fffed68c975d830117ca8fa95349
AMANDDHUMAL <129135638+AMN-D@users.noreply.github.com>
2024-02-25 12:34:22 +0530

    custom page text container done

--- price_optimisation/files/urls.py
+++ price_optimisation/files/urls.py
@@ -1,25 +1,7 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    #path('actual_vs_predicted/', views.actual_vs_predicted, name='actual_vs_predicted'),
-    #path('price_optimization/', views.price_optimization, name='price_optimization'),
-    #path('abt/', views.about, name='about'),
+    path('custom/', views.custom, name='custom'),
 ]
--- price_optimisation/files/views.py
+++ price_optimisation/files/views.py
@@ -30,90 +30,26 @@ def homepage(request):
 
     return render(request, 'base.html', context)
 
-'''
-import os
-import random 
-from pathlib import Path
-import pandas as pd
-from django.shortcuts import render, HttpResponse
-from django.template import loader
-import tensorflow as tf
-from sklearn.model_selection import train_test_split
-import json
-from django.http import JsonResponse
+def custom(request):
 
+    df = pd.read_csv(os.path.join(settings.BASE_DIR, 'files', 'data', 'preprocessed_price_optimization_dataset.csv'))
+    model_path = os.path.join(settings.BASE_DIR, 'files', 'data', 'price_optimization_model.h5')
 
-def actual_vs_predicted(request):
-    df, loaded_model = perform_price_optimization()
     X = df[['product_id', 'category_id', 'brand_encoded', 'historical_price_scaled']] 
     y = df['historical_price'] 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    loaded_model = load_model(model_path)
+
     predictions = loaded_model.predict(X_test)
     evaluation = loaded_model.evaluate(X_test, y_test)
+
     actual_data = list(y_test) 
     predicted_data = list(predictions.flatten()) 
-    zipped_data = list(zip(actual_data, predicted_data, df['product_id']))
+
     context = {
-        'zipped_data': zipped_data,
-        'evaluation': evaluation, 
         'actual_data': actual_data,
         'predicted_data': predicted_data,
     }
-    return render(request, 'actual_vs_predicted.html', context)
 
-def price_optimization(request):
-    if request.method == 'POST':
-        product_id = int(request.POST.get('product_id'))
-        category_id = int(request.POST.get('category_id'))
-        brand = int(request.POST.get('brand'))
-        historical_price = float(request.POST.get('historical_price'))
-        # Assuming you have the necessary feature encoding and scaling logic here
-        df, loaded_model = perform_price_optimization()
-        # Prepare the input features
-        X = [[product_id, category_id, brand, historical_price]]
-        # Make prediction
-        optimized_price = loaded_model.predict(X)[0][0]
-        # Pass the form data and prediction result to the template
-        return render(request, 'price_optimization.html', {
-            'show_result': True,
-            'product_id': product_id,
-            'category_id': category_id,
-            'brand': brand,
-            'historical_price': historical_price,
-            'optimized_price': optimized_price,
-        })
-    else:
-        return render(request, 'price_optimization.html', {'show_result': False})
-
-def about(request):
-  template = loader.get_template('about.html')
-  return HttpResponse(template.render())
-
-def perform_price_optimization():
-    BASE_DIR = Path(__file__).resolve().parent.parent
-    model_name = request.POST.get('model_name')
-    if model_name == 'lowkey':
-        model_path = os.path.join(BASE_DIR, 'files', 'data', 'price_optimization_model.h5')
-    else:
-        # Handle other model names or default behavior here
-        model_path = 'default_model_path'  # Replace with appropriate default
-
-    df = pd.read_csv(os.path.join(BASE_DIR, 'files', 'data', 'preprocessed_price_optimization_dataset.csv'))
-    loaded_model = tf.keras.models.load_model(model_path)
-
-    # Render the results (e.g., using a template)
-    context = {'results': results}  # Replace 'results' with your actual results
-    html_content = render_to_string('price_optimization_results.html', context)
-    return JsonResponse({'html_content': html_content})
-    
-    
-   # BASE_DIR = Path(__file__).resolve().parent.parent
-   # df = pd.read_csv(os.path.join(BASE_DIR, 'files', 'data', 'preprocessed_price_optimization_dataset.csv'))
-   # loaded_model = tf.keras.models.load_model(os.path.join(BASE_DIR, 'files', 'data', 'price_optimization_model.h5'))
-   # return df, loaded_model 
-
-'''
-
-
-
-    
+    return render(request, 'custom.html', context)
