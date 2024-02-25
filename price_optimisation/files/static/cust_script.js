091146110fa85afc296113e42ac259f6abbcd45c
AMANDDHUMAL <129135638+AMN-D@users.noreply.github.com>
2024-02-25 17:20:55 +0530

    custom page checkpoint

--- price_optimisation/files/static/cust_script.js
+++ price_optimisation/files/static/cust_script.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     var selectElement = document.getElementById('selectOption');
+    var containerDiv = document.querySelector('.body-your-inside-csv-container');
+    var paragraph = document.createElement('p'); // Create the paragraph element
 
     selectElement.addEventListener('change', function () {
         if (selectElement.value === '1') {
-            var containerDiv = document.querySelector('.body-your-inside-csv-container');
+            // Fill the table
             if (containerDiv) {
                 var jsonData = JSON.parse('{{ json_data|escapejs }}');
                 var table = '<table border="1">';
@@ -21,7 +23,19 @@ document.addEventListener("DOMContentLoaded", function () {
                 }
                 table += '</table>';
                 containerDiv.innerHTML = table;
+
+                // Remove the paragraph if it exists
+                if (containerDiv.contains(paragraph)) {
+                    containerDiv.removeChild(paragraph);
+                }
             }
+        } else if (selectElement.value === '0') {
+            // Clear the div
+            containerDiv.innerHTML = '';
+
+            // Add the paragraph
+            paragraph.textContent = 'CSV';
+            containerDiv.appendChild(paragraph);
         } else {
             console.log('Select a value');
         }
