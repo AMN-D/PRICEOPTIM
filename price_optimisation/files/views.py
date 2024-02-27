ddb3d27555329f994e7ee302b86246e7a4ad2b83
AMANDDHUMAL <129135638+AMN-D@users.noreply.github.com>
2024-02-27 19:57:37 +0530

    prediction is working
    un tested

--- price_optimisation/files/views.py
+++ price_optimisation/files/views.py
@@ -38,43 +38,37 @@ def custom(request):
     df = pd.read_csv(os.path.join(settings.BASE_DIR, 'files', 'data', 'preprocessed_price_optimization_dataset.csv'))
     json_data = df.to_json(orient='records')
     model_path = os.path.join(settings.BASE_DIR, 'files', 'data', 'price_optimization_model.h5')
+    loaded_model = load_model(model_path)
 
     X = df[['product_id', 'category_id', 'brand_encoded', 'historical_price_scaled']] 
     y = df['historical_price'] 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    loaded_model = load_model(model_path)
-
-    predictions = loaded_model.predict(X_test)
-    evaluation = loaded_model.evaluate(X_test, y_test)
-
-    actual_data = list(y_test) 
-    predicted_data = list(predictions.flatten()) 
-    
+     
     features = X.columns.tolist()
     outcome = y.name
 
     if request.method == 'POST':
         try:
-            received_dict = {}
             received_data = json.loads(request.body.decode('utf-8'))
-            for key, value in received_data.items():
-                received_dict[key] = value
-            print("Received dictionary:", received_dict)
 
-            for key, value in received_dict.items():
-                if key in X.columns:
-                    X[key] = pd.to_numeric(value)
+            # Get the values for each key and convert them to numeric types
+            product_id = int(received_data.get('product_id'))
+            category_id = int(received_data.get('category_id'))
+            brand_encoded = float(received_data.get('brand_encoded'))
+            historical_price_scaled = float(received_data.get('historical_price_scaled'))
 
-            print(X)
+            # Print the converted values
+            print("Product ID:", product_id)
+            print("Category ID:", category_id)
+            print("Brand Encoded:", brand_encoded)
+            print("Historical Price Scaled:", historical_price_scaled)
 
-            predictions = loaded_model.predict(X)
-            evaluation = loaded_model.evaluate(X, y_test)
-            predicted_data = predictions.flatten().tolist()
+            
+            X = pd.DataFrame([[product_id, category_id, brand_encoded, historical_price_scaled]],
+                         columns=['product_id', 'category_id', 'brand_encoded', 'historical_price_scaled'])
+            optimized_price = loaded_model.predict(X)[0][0]
 
-            print(predicted_data)
+            print(optimized_price)
 
-            return JsonResponse({'message': 'Received data successfully'}, status=200)
         except json.JSONDecodeError as e:
             return JsonResponse({'error': 'Failed to decode JSON data'}, status=400)
 
