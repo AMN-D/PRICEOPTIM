16e226f86f17b176f00e136888374927ef91dfc3
AMANDDHUMAL <129135638+AMN-D@users.noreply.github.com>
2024-02-29 18:40:10 +0530

    updated settings .py

--- price_optimisation/config/settings.py
+++ price_optimisation/config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+0-!8e47+k(hwsb4)t%et_3f)3j)*dk5-f!0mfu%$2atur@3@+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
